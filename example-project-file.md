408c04faaad8336e4990bd66188893370c5508e9
Nick Byrne <byrnen8@tcd.ie>
2021-03-19 10:39:13 +0000

    Added attachments for parallelisation step of PDAF. PDAF interfaces and callback routines are located in the pdaf_bindings subdirectory and chages to NEMO source in nemo_src. Added basic build script for ARCHER2. Added basic project file for FORD document generator. Modified precommit so that Fortran linter only operates on pdaf_bindings subdirectory (don't want to reformat NEMO source code, only pure PDAF modifications).

--- example-project-file.md
+++
@@ -1,5 +0,0 @@
-project: fortran_dev
-summary: a wrapper for developing in fortran
-author: nenb
-src_dir: ./src
-output_dir: ./doc
---
+++ src/nemo_r4.0.4/build.sh
@@ -0,0 +1,36 @@
+#! /bin/sh -
+
+module ()
+{
+    eval `/usr/local/Modules/bin/modulecmd bash $*`
+}
+
+# env for CCE build
+module -s restore /work/n01/shared/acc/n01_modules/ucx_env
+
+touch compile_log.txt
+
+# save module info
+{
+    module list
+} |& tee -a compile_log.txt
+
+# remove previous build, can cause build errors otherwise
+{
+    echo y | ./makenemo -n "${MY_CFG}" -r ORCA2_ICE_PISCES -m "${MY_ARCH}" -j 0 clean_config
+} |& tee -a compile_log.txt
+
+# Create directory for source code modifications
+if [ ! -d "./MY_SRC" ]; then
+  mkdir MY_SRC
+fi
+cp -a "${PDAF_BIND_SRC}/pdaf_bindings/." ./MY_SRC/
+cp -a "${PDAF_BIND_SRC}/nemo_src/." ./MY_SRC/
+CWD=$(pwd)
+export MY_SRC=${CWD}/MY_SRC
+
+# create new NEMO config based on ORCA2_ICE_PISCES config,
+# add source code modifications, build!
+{
+    ./makenemo -n "${MY_CFG}" -e "${MY_SRC}" -r ORCA2_ICE_PISCES -m "${MY_ARCH}" add_key 'key_USE_PDAF' -j 16
+} |& tee -a compile_log.txt
