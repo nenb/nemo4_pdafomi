1570a5cd1adeb220f27b7a6515314bbd1e343edb
nenb <55434794+nenb@users.noreply.github.com>
2021-03-19 11:53:05 +0000

    Deploying to gh-pages from @ nenb/nemo4_pdafomi@0c6a89c45c471186647524341387afe9a90cf665 🚀

---
+++ src/mod_kind_pdaf.F90
@@ -0,0 +1,16 @@
+MODULE mod_kind_pdaf
+
+!>## Define real precision
+!>
+!>This module defines the kind of real and length of character strings
+!>for the PDAF call-back routines and interfaces. It is based on the NEMO
+!>module `par_kind.F90`.
+
+   IMPLICIT NONE
+   SAVE
+
+   INTEGER, PARAMETER :: pdp = SELECTED_REAL_KIND(12, 307)
+   INTEGER, PARAMETER :: pwp = pdp
+   INTEGER, PARAMETER :: lc = 256
+
+END MODULE mod_kind_pdaf
